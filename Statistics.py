2c20d227aee6b15824b4338f1370c7de2ca96e26
Ninju <128250644+nxxju@users.noreply.github.com>
2024-12-18 21:10:48 +0530

    Create Statistics.py

---
+++ Statistics.py
@@ -0,0 +1,11 @@
+import statistics
+
+ages = [25, 30, 35, 40, 45, 50, 55, 60, 70]
+
+x = statistics.mean(ages)
+y = statistics.median(ages)
+z = statistics.mode(ages)
+
+print("Mean", x)
+print("Median", y)
+print("Median", z)
